57b44a511a9002b7cdab848dff6b9880659ac092
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-03-18 11:22:26 +0300

    updated index.js as instructed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left -1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Arrowleft") {
+        moveDodgerLeft();
+    }
+});
+function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 40) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}   
