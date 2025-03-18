a6aa9485690c6cdfaf4d72d41e58c8eaa6e21ba1
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-03-18 11:25:29 +0300

    correct code

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
 
-    if (left < 40) {
+    if (left < 360) {
         dodger.style.left = `${left + 1}px`;
     }
 }   
